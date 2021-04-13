599bba89614981c9fa862da5dffdfffff6e5f252
igor4ikc <igor4ikc@users.noreply.github.com>
2021-04-13 21:49:48 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../3.2/Man.h"
+#include "../3.2/Man.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Man a("name", 10);
+			Assert::AreEqual(a.getA(), 10);
+		}
+	};
+}
